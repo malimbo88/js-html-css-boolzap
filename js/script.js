47378f84e51e7d4da7fff49204338ec637d4e5ff
Nicola Valente <nicolavalente88@gmail.com>
2020-06-21 11:45:46 +0200

    Correzioni

--- js/script.js
+++ js/script.js
@@ -1,20 +1,19 @@
 //myJavaScript
 $(document).ready(function() {
 
-  //Quando faccio mouseenter sulla input send message bar appare icona sent_message
+  //Quando faccio focus sulla input send message bar appare icona sent_message
   //aggiungo la classe none dal div che deve scomparire
   //tolgo classe non da div che devo mostrare
-  $(document).on("mouseenter", "#send_message_bar", function () {
-    $(".rigth_switch.icon").children(".sent_icon").siblings().addClass("none");
+  $(document).on("focus", "input#send_message_bar", function () {
+    $(".rigth_switch.icon").children(".mic_icon").addClass("none");
     $(".rigth_switch.icon").children(".sent_icon").removeClass("none");
   });
 
-
-  //Quando faccio mouseleave sulla texting_bar generale appare icona mic_icon
+  //Quando faccio tolgo il focus(blur) sulla input#send_message_bar appare icona mic_icon
   //aggiungo la classe none dal div che deve scomparire
   //tolgo classe non da div che devo mostrare
-  $(document).on("mouseleave", ".texting_bar", function () {
-    $(".rigth_switch.icon").children(".sent_icon").siblings().removeClass("none");
+  $(document).on("blur", ".texting_bar", function () {
+    $(".rigth_switch.icon").children(".mic_icon").removeClass("none");
     $(".rigth_switch.icon").children(".sent_icon").addClass("none");
   });
 
@@ -23,14 +22,14 @@ $(document).ready(function() {
     sendMessage();
   });
 
-  //invio messaggio schiacciando su pulsante invio di tastiera
+  //Invio messaggio schiacciando su pulsante invio di tastiera
   $(document).on("keypress", "#send_message_bar", function() {
     if (event.which === 13 || event.keyCode === 13) {
       sendMessage();
     }
   });
 
-  //cercare contatti con input search
+  //Cercare contatti con input search
   $(document).on("keyup", "#search_bar", function() {
     var userType = $(this).val().toLowerCase()
     //cerca un nome nei contatti attraverso il valore input iserito in search_bar
@@ -46,6 +45,7 @@ $(document).ready(function() {
       }
     });
   });
+
   //se faccio mouseenter su square_speech messaggio appare icona menu square_speech_dropdown
   //per rendere visibile o meno aggiungo e tolgo classe none
   $(document).on("mouseenter", ".square_speech", function() {
@@ -110,14 +110,13 @@ $(document).ready(function() {
   });
 
 //FUNZIONI
-//Invia un messaggio e aggiungilo al div con classe messages_box
+//Invia un messaggio e aggiungilo al div con classe messages_box (ovvero la finestra chat)
 function sendMessage() {
-  // vado a definire in una variabile quello che utente scrive in input text_area
+  // vado a inserire in una variabile quello che utente scrive in input text_area (ovvero il .val di input)
   var messageText = $("#send_message_bar").val();
-
   // se input text_area non è vuoto proseguo appendendo il messaggio inviato
   if (messageText != "") {
-    // indico una variabile che definisce il contenitore del testo scritto da utente
+    // indico una variabile che definisce il contenitore (square_speech) del testo scritto da utente
     //ovvero .square_speech che è appunto il div contenitore del tag span contenete il message_text
     var newMessageSquare = $(".templates .square_speech").clone();
     //scrivo il testo che è il val di input, ovvero ciò che utente ha scritto nel div span message_text
@@ -126,43 +125,45 @@ function sendMessage() {
     newMessageSquare.children(".message_time").text(time());
     //resetto il valore di input in modo che utente possa scrivere un nuovo messaggio
     $(".texting_bar input").val("");
-    //aggiungo al contenitore del testo la classe sent per dargli li stili dei messaggi inviati
+    //aggiungo al contenitore del testo la classe sent per dargli gli stili dei messaggi inviati
     newMessageSquare.addClass("sent");
     //appendo il div nel message_box ovvero dove utente potrà leggere i messaggi inviati e ricevuti
+    //appendo il messaggio nella finestra di conversazione relativa al contatto corrente (ovvero quella con classe.active)
     $(".messages_box.active").append(newMessageSquare);
-    //Invia messaggio di risposta
+    //Genero messaggio di riasposta tramite funzione sendMessageReply
+    //Invio messaggio di risposta dopo x secondi dal messaggio inviato da utente
     setTimeout(sendMessageReply, 1000);
-    //scrollo la message_box fino alla fine per mostrare a utente i messaggi piu recenti
-    var messageBoxHigh = $(".texting_area").height();
+    //scrollo la message_box fino alla fine ovvero altezza totale del div contenitore texting_area per mostrare a utente i messaggi piu recenti
+    var messageBoxHigh = $(".texting_area").prop("scrollHeigth");
     $(".texting_area").scrollTop(messageBoxHigh)
   }
-}
+};
 
 //Invia messaggio di risposta
 function sendMessageReply() {
-
-  // indico una variabile che definisce il contenitore del testo scritto da utente
+  // indico una variabile che definisce il contenitore del testo di rsiposta
   //ovvero .square_speech che è appunto il div contenitore del tag span contenete il message_text
   var newMessageSquare = $(".templates .square_speech").clone();
   //scrivo il testo che è Ciao
   newMessageSquare.children(".message_text").text("Ciao");
   //scrivo l'orario corrente sotto al messaggio
   newMessageSquare.children(".message_time").text(time());
-  //aggiungo al contenitore del testo la classe sent per dargli li stili dei messaggi inviati
+  //aggiungo al contenitore del testo la classe received per dargli gli stili dei messaggi ricevuti
   newMessageSquare.addClass("received");
   //appendo il div nel message_box ovvero dove utente potrà leggere i messaggi inviati e ricevuti
   $(".messages_box.active").append(newMessageSquare);
-
-}
+};
 
 //Determina orario corrente
 //scrivi orario corrente aggiungendo gli 0 davanti alle cifre inferiori a 10
+//ritorna orario corrente
 function time() {
   var date = new Date()
   var hours = date.getHours()
   var minutes = date.getMinutes()
   var currentTime = hours + ":"+ minutes;
-
+  //se il numero generato e`inferiore a 10
+  //aggiungo lo 0 davanti al numero indicante ora e minuti correnti
   if (hours < 10) {
     currentTime = "0" + hours + ":" + minutes;
   }else if (minutes < 10) {
@@ -171,6 +172,7 @@ function time() {
       currentTime = "0" + hours + ":" + "0" + minutes;
   }
   return currentTime
-}
+};
 
+//Fine document.ready Jquery
 });
